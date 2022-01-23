bb64e2ef5f653162ecdc9e0b0e544f26b578ba6c
Max Ratajczak <75450072+maxratajczak@users.noreply.github.com>
2022-01-22 20:20:06 -0500

    Lightbox

---
+++ assets/javascript/lightBox.js
@@ -0,0 +1,8 @@
+var lightbox = new SimpleLightbox('.gallery a', {
+    scrollZoomFactor: 0.1,
+    animationSlide: false,
+    history: false,
+    captions: false,
+    disableRightClick: true,
+    fadeSpeed: 125
+});
